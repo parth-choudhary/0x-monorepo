12ce688ba8d8f274b18d2da65e719599897702a7
Amir Bandeali <abandeali1@gmail.com>
2018-02-18 02:33:59 -0700

    Add marketFillOrders

--- packages/contracts/src/contracts/current/protocol/Forwarder/Forwarder.sol
+++ packages/contracts/src/contracts/current/protocol/Forwarder/Forwarder.sol
@@ -2,9 +2,7 @@ pragma solidity ^0.4.19;
 
 import "../Exchange/Exchange.sol";
 import "../TokenTransferProxy/TokenTransferProxy.sol";
-// import "../../tokens/Token.sol";
 import { WETH9 as EtherToken } from "../../tokens/WETH9.sol";
-// import "../../utils/SafeMath/SafeMath.sol";
 
 contract Forwarder is SafeMath {
 
@@ -35,15 +33,45 @@ contract Forwarder is SafeMath {
         uint8 v,
         bytes32 r,
         bytes32 s)
-        public
+        external
         payable
     {
         require(msg.value > 0);
         require(orderAddresses[3] == address(etherToken));  // takerToken must be etherToken
         
         uint256 takerTokenFillAmount = msg.value;
-        etherToken.deposit.value(msg.value);
+        etherToken.deposit.value(takerTokenFillAmount);
+
         require(exchange.fillOrder(
+            orderAddresses,
+            orderValues,
+            takerTokenFillAmount,
+            true,   // always throw on failed transfer
+            v,
+            r,
+            s
+        ) == takerTokenFillAmount);
+
+        uint256 makerTokenFilledAmount = getPartialAmount(orderValues[0], orderValues[1], takerTokenFillAmount);    // makerTokenAmount * takerTokenFillAmount / takerTokenAmount
+        require(Token(orderAddresses[0]).transfer(msg.sender, makerTokenFilledAmount));
+    }
+
+    function marketFillOrders(
+        address[5][] orderAddresses,
+        uint[6][] orderValues,
+        uint8[] v,
+        bytes32[] r,
+        bytes32[] s)
+        external
+        payable
+    {
+        require(msg.value > 0);
+
+        uint256 takerTokenFillAmount = msg.value;
+        etherToken.deposit.value(takerTokenFillAmount);
+
+        // Note: We assume that takerToken of all orders is EtherToken
+        require(exchange.fillOrdersUpTo(
             orderAddresses,
             orderValues,
             takerTokenFillAmount,
