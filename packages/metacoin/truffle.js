8064ae7982fb837bfb7afb8676d0512ec775b22b
Leonid Logvinov <logvinov.leon@gmail.com>
2018-03-13 15:11:38 +0100

    Pass CoverageProvider to truffle

--- packages/metacoin/truffle.js
+++ packages/metacoin/truffle.js
@@ -1,4 +1,16 @@
+const { CoverageProvider } = require('@0xproject/sol-cov');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+    networks: {
+        development: {
+            provider: new CoverageProvider(
+                'build/contracts',
+                'contracts',
+                50,
+                '0x5409ed021d9299bf6814279a6a1411a7e866a631',
+                'https://ropsten.infura.io/',
+            ),
+            network_id: 50,
+        },
+    },
 };
