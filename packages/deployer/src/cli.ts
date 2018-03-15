01cb2e02898e8f7a62edd777bf9f786a5d7f180d
Leonid Logvinov <logvinov.leon@gmail.com>
2018-03-15 16:29:55 +0100

    Stop printing help on error

--- packages/deployer/src/cli.ts
+++ packages/deployer/src/cli.ts
@@ -7,6 +7,7 @@ import * as yargs from 'yargs';
 
 import { commands } from './commands';
 import { constants } from './utils/constants';
+import { consoleReporter } from './utils/error_reporter';
 import { CliOptions, CompilerOptions, DeployerOptions } from './utils/types';
 
 const DEFAULT_OPTIMIZER_ENABLED = false;
@@ -139,12 +140,12 @@ function deployCommandBuilder(yargsInstance: any) {
             default: DEFAULT_CONTRACTS_LIST,
             description: 'comma separated list of contracts to compile',
         })
-        .command('compile', 'compile contracts', identityCommandBuilder, onCompileCommandAsync)
+        .command('compile', 'compile contracts', identityCommandBuilder, consoleReporter(onCompileCommandAsync))
         .command(
             'deploy',
             'deploy a single contract with provided arguments',
             deployCommandBuilder,
-            onDeployCommandAsync,
+            consoleReporter(onDeployCommandAsync),
         )
         .help().argv;
 })();
